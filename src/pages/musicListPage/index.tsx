0ff627b60eadd5ced56c541fa312ce0533326514
GitLabSJ <aeeou12@gmail.com>
2023-05-01 15:06:02 +0900

    feat: musicList Header 세팅

--- src/pages/musicListPage/index.tsx
+++ src/pages/musicListPage/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import styled from 'styled-components';
+import Header from './components/Header';
+
+const Wrapper = styled.div`
+    height: 100vh;
+    width: 100%;
+    background: #2a1e57;
+`;
+
+const FilterWrap = styled.div`
+    height: 5%;
+    width: 100%;
+    background-color: orange;
+`;
+
+const ContentWrap = styled.div`
+    height: 55%;
+    background-color: yellow;
+`;
 
 const MusicListPage = () => {
-  return (
-    <div>index</div>
-  )
-}
+    return (
+        <Wrapper>
+            <Header></Header>
+            <FilterWrap />
+            <ContentWrap />
+        </Wrapper>
+    );
+};
 
-export default MusicListPage
+export default MusicListPage;
