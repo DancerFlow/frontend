8af0678f36adf9deec4b34322603a313b0d54452
[AI6/1반] 최호열 <ichy1004@gmail.com>
2023-05-01 17:18:57 +0900

    Feat :  메인페이지 화면 구성과 로그인, 회원가입 폼 구성

---
+++ src/pages/mainPage/LoginModal.tsx
@@ -0,0 +1,189 @@
+import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
+import DragDrop from '../../components/DragDrop';
+
+interface Props {
+    setIsClicked: React.Dispatch<React.SetStateAction<boolean>>;
+    setIsHover: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoginModal = ({ setIsClicked, setIsHover }: Props) => {
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const [formValid, setFormValid] = useState<string>('');
+    const [file, setFile] = useState<FileList | undefined>();
+    const [registerStep, setRegisterStep] = useState(0);
+
+    const handleModalClick = () => {
+        setIsClicked(false);
+        setIsHover(false);
+        setRegisterStep(0);
+    };
+
+    const handleRegisterStep = () => {
+        switch (registerStep) {
+            case 0:
+                return (
+                    <>
+                        <FieldContainer>
+                            <Fieldset>
+                                <input placeholder="enter email" required ref={emailRef} id="email" type="email" name="email" />
+                            </Fieldset>
+
+                            <Fieldset>
+                                <input
+                                    required
+                                    ref={passwordRef}
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    placeholder="enter password"
+                                />
+                            </Fieldset>
+
+                            <LoginButton>LOGIN</LoginButton>
+                            <p>{formValid}</p>
+                        </FieldContainer>
+                        <SelectContainer>
+                            <a onClick={() => setRegisterStep(1)}>sign up</a>
+                            <a>join as a guest</a>
+                        </SelectContainer>
+                    </>
+                );
+            case 1:
+                return (
+                    <FieldContainer>
+                        <Fieldset>
+                            <input placeholder="enter email" required ref={emailRef} id="email" type="email" name="email" />
+                        </Fieldset>
+
+                        <Fieldset>
+                            <input required ref={passwordRef} id="password" type="password" name="password" placeholder="enter password" />
+                        </Fieldset>
+
+                        <Fieldset>
+                            <input required ref={passwordRef} id="password" type="password" name="password" placeholder="enter password" />
+                        </Fieldset>
+
+                        <Fieldset>
+                            <input required ref={passwordRef} id="password" type="password" name="password" placeholder="enter password" />
+                        </Fieldset>
+
+                        <LoginButton onClick={() => setRegisterStep(2)}>NEXT</LoginButton>
+                        <p>{formValid}</p>
+                    </FieldContainer>
+                );
+            case 2:
+                return (
+                    <FieldContainer>
+                        <Fieldset>
+                            <DragDrop file={file} setFile={setFile}></DragDrop>
+                        </Fieldset>
+
+                        <LoginButton>SIGNUP</LoginButton>
+                        <p>{formValid}</p>
+                    </FieldContainer>
+                );
+        }
+    };
+
+    return (
+        <>
+            <ModalBackground onClick={() => handleModalClick()}> </ModalBackground>
+            <FormS>
+                <FormContainer>{handleRegisterStep()}</FormContainer>
+            </FormS>
+        </>
+    );
+};
+
+const ModalBackground = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 99;
+    width: 100vw;
+    height: 100vh;
+    background-color: #00000042;
+`;
+const FormS = styled.div`
+    position: absolute;
+    z-index: 100;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    align-items: center;
+    justify-content: center;
+    display: flex;
+`;
+const FormContainer = styled.div`
+    display: flex;
+    padding: 50px 20px 50px 20px;
+    border-radius: 4px;
+    width: 500px;
+    box-shadow: 2px 2px 5px rgb(0, 0, 0, 0.2);
+    flex-direction: column;
+    background-color: white;
+    align-items: center;
+
+    input {
+        width: 100%;
+        height: 40px;
+        border-radius: 0px;
+        border-style: solid;
+        border-width: 1px;
+        padding: 5px 0px;
+        text-indent: 6px;
+        margin-top: 10px;
+        margin-bottom: 20px;
+        font-size: 0.9rem;
+        letter-spacing: 2px;
+    }
+`;
+
+const FieldContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Fieldset = styled.fieldset`
+    text-transform: uppercase;
+    border-style: none;
+    width: 400px;
+    margin: 0;
+    padding: 0;
+    border: none;
+`;
+const LoginButton = styled.button.attrs({ type: 'submit' })`
+    height: 40px;
+    width: 200px;
+    border-radius: 0px;
+    border-style: none;
+    background-color: #27fd1c;
+
+    padding: 8px 20px;
+    letter-spacing: 0.8px;
+    display: block;
+    margin-top: 10px;
+    box-shadow: 2px 2px 2px rgb(0, 0, 0, 0.1);
+    cursor: pointer;
+    &:hover {
+        opacity: 0.8;
+    }
+`;
+
+const SelectContainer = styled.div`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 10px;
+    a {
+        margin: 0px 10px;
+        border: solid #27fd1c;
+        border-width: 0 0 1px 0;
+        cursor: pointer;
+    }
+`;
+
+export default LoginModal;
--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,7 +1,193 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import styled, { keyframes } from 'styled-components';
+import Lottie from 'lottie-react';
+import ReactHowler from 'react-howler';
+
+import LoginModal from './LoginModal';
+
+import bgm from './source/rukunetsu.mp3';
+import bgVideo from './source/dancerflow.mp4';
+import bgmOnOff from './source/bgmOnOff.json';
 
 const MainPage = () => {
-    return <div>메인됩니까요?</div>;
+    const [isHover, setIsHover] = useState<boolean>(false);
+    const [isClicked, setIsClicked] = useState<boolean>(false);
+    const [isSoundOn, setIsSoundOn] = useState<boolean>(false);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const lottieRef = useRef<any>(null);
+
+    //버튼 호버시 배경동영상 재생 컨트롤
+    useEffect(() => {
+        if (videoRef.current == null) return;
+        if (isClicked) return;
+
+        if (isHover) {
+            videoRef.current.muted = true;
+            videoRef.current.play();
+            return;
+        }
+        videoRef.current.muted = true;
+        const reversePlay = setInterval(() => {
+            if (videoRef.current.currentTime < 0.01) {
+                clearInterval(reversePlay);
+                videoRef.current.pause();
+            } else {
+                videoRef.current.play();
+                videoRef.current.currentTime -= 0.05;
+            }
+        }, 40);
+    }, [isHover, isClicked]);
+
+    useEffect(() => {
+        isSoundOn ? lottieRef.current.playSegments([33, 67], true) : lottieRef.current.playSegments([1, 33], true);
+    }, [isSoundOn]);
+
+    const onTimeUpdate = () => {
+        if (!isClicked && videoRef.current?.currentTime >= 0.2) {
+            videoRef.current?.pause();
+        }
+    };
+
+    const handleOnClick = () => {
+        setIsClicked(true);
+        videoRef.current?.play();
+    };
+
+    const handleSound = () => {
+        setIsSoundOn((cur) => !cur);
+    };
+
+    return (
+        <div>
+            <BackgroundContainer>
+                <ConfirmSoundPlay onClick={(e) => e.currentTarget.classList.add('clear')}>
+                    <span>신나는 브금이랑 함께 하실래요?</span>
+                    <div
+                        onClick={() => {
+                            setIsSoundOn(true);
+                        }}
+                    >
+                        넹!
+                    </div>
+                    <div
+                        onClick={() => {
+                            setIsSoundOn(false);
+                        }}
+                    >
+                        즐~
+                    </div>
+                </ConfirmSoundPlay>
+                <VideoS
+                    ref={videoRef}
+                    onTimeUpdate={onTimeUpdate}
+                    loop={true}
+                    onMouseOver={() => setIsHover(true)}
+                    onMouseOut={() => setIsHover(false)}
+                    onClick={handleOnClick}
+                >
+                    <source src={bgVideo} type="video/mp4"></source>
+                </VideoS>
+                {/* <ButtonS onMouseOver={() => setIsHover(true)} onMouseOut={() => setIsHover(false)} onClick={handleOnClick}>
+                    GAME START
+                </ButtonS> */}
+                {isClicked && <LoginModal setIsClicked={setIsClicked} setIsHover={setIsHover} />}
+            </BackgroundContainer>
+
+            <ReactHowler src={[bgm]} playing={isSoundOn} volume={0.2} loop={true} />
+
+            <BgmController onClick={handleSound}>
+                <Lottie lottieRef={lottieRef} animationData={bgmOnOff} loop={false} autoPlay={false}></Lottie>
+            </BgmController>
+        </div>
+    );
 };
+
+const BackgroundContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: black;
+    overflow: hidden;
+`;
+
+const ConfirmKeyframes = keyframes`
+    0%{
+        top: 90%;
+        transform: translate(-50%, -50%) rotate(0deg);
+    }
+    70%{
+        top: 70%;
+        transform: translate(-50%, -50%) rotate(-3deg);
+    }
+    90%{
+        top: 55%;
+        transform: translate(-50%, -50%) rotate(-4deg);
+    }
+    100%{
+        top: 50%;
+        transform: translate(-50%, -50%) rotate(-5deg);
+    }
+`;
+
+const ConfirmSoundPlay = styled.div`
+    z-index: 99;
+    display: flex;
+    position: absolute;
+    width: 680px;
+    height: 120px;
+    background-color: #27fd1c;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(-5deg);
+    animation: ${ConfirmKeyframes} 0.3s linear 0s 1;
+    align-items: center;
+    font-family: 'NanumSquareNeoBold';
+    transition: 0.3s ease-out;
+    span {
+        margin-right: 80px;
+        margin-left: 150px;
+    }
+    div {
+        width: 60px;
+        height: 60px;
+        background-color: white;
+        line-height: 60px;
+        margin-left: 30px;
+        border-radius: 40px;
+        cursor: pointer;
+    }
+    &.clear {
+        visibility: hidden;
+        opacity: 0;
+    }
+`;
+
+const VideoS = styled.video`
+    margin-top: 100px;
+    width: 1200px;
+    cursor: pointer;
+`;
+
+const ButtonS = styled.button`
+    width: 200px;
+    height: 80px;
+    position: absolute;
+    top: 70%;
+    left: 50%;
+    font-family: 'NanumSquareNeoExtraBold';
+    background-color: #0223ff;
+    color: white;
+    transform: translate(-50%, -50%);
+`;
+
+const BgmController = styled.div`
+    position: absolute;
+    bottom: 30px;
+    right: 30px;
+    width: 50px;
+    height: 50px;
+    border-radius: 100px;
+    background-color: #27fd1c;
+    cursor: pointer;
+`;
 
 export default MainPage;
