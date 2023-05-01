2b2e117ecf9ac5c638f5cbece389e4f4429ca438
GitLabSJ <aeeou12@gmail.com>
2023-05-01 16:29:44 +0900

    feat: musicList Filter 세팅

--- src/pages/musicListPage/index.tsx
+++ src/pages/musicListPage/index.tsx
@@ -1,17 +1,6 @@
 import styled from 'styled-components';
 import Header from './components/Header';
-
-const Wrapper = styled.div`
-    height: 100vh;
-    width: 100%;
-    background: #2a1e57;
-`;
-
-const FilterWrap = styled.div`
-    height: 5%;
-    width: 100%;
-    background-color: orange;
-`;
+import Filter from './components/Filter';
 
 const ContentWrap = styled.div`
     height: 55%;
@@ -22,10 +11,16 @@ const MusicListPage = () => {
     return (
         <Wrapper>
             <Header></Header>
-            <FilterWrap />
+            <Filter />
             <ContentWrap />
         </Wrapper>
     );
 };
+
+const Wrapper = styled.div`
+    height: 100vh;
+    width: 100%;
+    background: #2a1e57;
+`;
 
 export default MusicListPage;
