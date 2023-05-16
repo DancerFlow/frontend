5cd711c7ce4d3f47aa4289c57bc84a5685ea3702
GitLabSJ <aeeou12@gmail.com>
2023-05-16 20:36:12 +0900

    fix: 정답추출 수정

--- src/pages/challengePage/Score/Score.tsx
+++ src/pages/challengePage/Score/Score.tsx
@@ -3,12 +3,11 @@ import * as poseDetection from '@tensorflow-models/pose-detection';
 import * as tf from '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-backend-webgl';
 import styled from 'styled-components';
-import testVideo from '../../../assets/fearless.mp4';
-import 춤예시 from '../../../assets/춤예시.mp4';
+
 const Score = () => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const [isPlaying, setIsPlaying] = useState(false);
-    let lastTime = -1; // 마지막으로 keypoints를 추출한 시간
+    let lastTime = -1;
 
     useEffect(() => {
         const runPoseEstimation = async () => {
@@ -20,31 +19,34 @@ const Score = () => {
                 minPoseScore: 0.2
             };
             const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
-            const keypointsArray: any = []; // 추출한 keypoints를 담을 배열
-            if (videoRef.current) {
-                videoRef.current.src = 춤예시;
+            const keypointsArray: any = [];
 
-                // 'timeupdate' 이벤트 리스너 추가
+            if (videoRef.current) {
                 videoRef.current.addEventListener('timeupdate', async () => {
                     if (videoRef.current) {
-                        const currentTime = Math.round(videoRef.current.currentTime * 2) / 2; // 현재 시간을 0.5초 단위로 반올림
+                        const currentTime = Math.round(videoRef.current.currentTime * 2) / 2;
 
-                        // 현재 시간이 마지막으로 keypoints를 추출한 시간보다 크다면
                         if (currentTime > lastTime) {
-                            lastTime = currentTime; // 마지막으로 keypoints를 추출한 시간을 업데이트
+                            lastTime = currentTime;
                             const pose = await detector.estimatePoses(videoRef.current, { maxPoses: 1 });
-                            // const validKeypoints = pose[0].keypoints.filter((keypoint) => keypoint.score > 0);
                             const validKeypoints = pose[0].keypoints;
                             keypointsArray.push({
                                 time: currentTime,
                                 keypoints: validKeypoints
-                            }); // keypointsArray에 현재 시간과 추출한 keypoints를 추가
+                            });
                         }
                     }
                 });
-                // 'ended' 이벤트 리스너 추가
+
                 videoRef.current.addEventListener('ended', () => {
-                    console.log(keypointsArray); // 비디오가 끝났을 때 keypointsArray를 콘솔에 출력
+                    console.log(keypointsArray);
+                    const dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(keypointsArray));
+                    const downloadAnchorNode = document.createElement('a');
+                    downloadAnchorNode.setAttribute('href', dataStr);
+                    downloadAnchorNode.setAttribute('download', 'keypoints.json');
+                    document.body.appendChild(downloadAnchorNode);
+                    downloadAnchorNode.click();
+                    downloadAnchorNode.remove();
                 });
             }
         };
@@ -61,14 +63,24 @@ const Score = () => {
         }
     };
 
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files?.length) {
+            const file = event.target.files[0];
+            const fileURL = URL.createObjectURL(file);
+            if (videoRef.current) {
+                videoRef.current.src = fileURL;
+            }
+        }
+    };
+
     return (
         <Container>
             <Video ref={videoRef}></Video>
             <button onClick={handlePlayButton}>정답추출</button>
+            <input type="file" accept="video/*" onChange={handleFileChange} />
         </Container>
     );
 };
-
 const Container = styled.div`
     height: 50%;
     width: 100%;
