5af5706409b9471bc8e9c209ff0092caef444b9d
GitLabSJ <aeeou12@gmail.com>
2023-08-14 01:25:04 +0900

    Refactor: canvas carmera 속성값 상수로 관리

--- src/pages/modePage/three/index.jsx
+++ src/pages/modePage/three/index.jsx
@@ -13,32 +13,31 @@ import Construction from './Construction';
 import Room from './Room';
 import LoadingView from '../../../components/common/LoadingView';
 
+const FLOOR_WIDTH = 30;
+const FLOOR_HEIGHT = 30;
+const FLOOR_CENTER = [0, -1, 0];
+
+const CAMERA_PROPS = {
+    position: [FLOOR_CENTER[0], FLOOR_CENTER[1] + 15, FLOOR_CENTER[2]],
+    fov: 50,
+    zoom: 40,
+    left: -FLOOR_WIDTH / 2,
+    right: FLOOR_HEIGHT / 2,
+    top: FLOOR_HEIGHT / 2,
+    bottom: -FLOOR_HEIGHT / 2,
+    near: -1000,
+    far: 1000
+};
+
 const Three = () => {
     const [destinationPoint, setDestinatioPoint] = useState([0, 0, -2]);
     const [playerAnimation, setPlayerAnimation] = useState(0);
     const [area, setArea] = useState(-1); // 0: practice, 1: challenge, 2: construction, 3: room
     const [isLoading, setIsLoading] = useState(true);
     const roomRef = useRef();
-    const floorWidth = 30;
-    const floorHeight = 30;
-    const floorCenter = [0, -1, 0];
 
     return (
-        <Canvas
-            orthographic={true}
-            shadows
-            camera={{
-                position: [floorCenter[0], floorCenter[1] + 15, floorCenter[2]],
-                fov: 50,
-                zoom: 40,
-                left: -floorWidth / 2,
-                right: floorWidth / 2,
-                top: floorHeight / 2,
-                bottom: -floorHeight / 2,
-                near: -1000,
-                far: 1000
-            }}
-        >
+        <Canvas orthographic={true} shadows camera={CAMERA_PROPS}>
             <ambientLight intensity={0.5} />
             <directionalLight
                 position={[10, 30, 10]}
@@ -59,7 +58,7 @@ const Three = () => {
                     </Html>
                 }
             >
-                <Floor setDestinatioPoint={setDestinatioPoint} width={floorWidth} height={floorHeight} center={floorCenter}></Floor>
+                <Floor setDestinatioPoint={setDestinatioPoint} width={FLOOR_WIDTH} height={FLOOR_HEIGHT} center={FLOOR_CENTER}></Floor>
                 <Construction area={area}></Construction>
                 <Practice area={area}></Practice>
                 <Room area={area}></Room>
